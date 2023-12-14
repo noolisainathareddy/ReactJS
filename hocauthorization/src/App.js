606044cbf124302e569ce520cd2ab4bf697059a7
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2023-12-14 07:58:39 -0600

    HOC concept

---
+++ hocauthorization/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hoc from "./Hoc";
+ 
+const App = () => {
+  return(
+    <div>
+      <center>
+        <h1> Welcome User</h1>
+      </center>
+    </div>
+  )
+}
+
+export default Hoc(App); 
---
+++ hocauthorization/src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hoc from './Hoc';
+
+const Login = () => {
+    return(
+        <div>
+            <center>
+                <h1>Please Login!!!</h1>
+            </center>
+        </div>
+    )
+}
+
+export default Hoc(Login);
